7e283a2aabe190bcbc52f345c284a700c68f93d8
黄勇 <huangyong.java@gmail.com>
2014-06-09 12:21:32 +0800

    修复了代码重构时产生的 Bug

--- soap_spring_cxf_servlet/src/test/java/demo/cxf/soap_spring_cxf_servlet/Client.java
+++ soap_spring_cxf_servlet/src/test/java/demo/cxf/soap_spring_cxf_servlet/Client.java
@@ -8,7 +8,7 @@ public class Client {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("spring-client.xml");
 
-        HelloService helloService = context.getBean("greetingService", HelloService.class);
+        HelloService helloService = context.getBean("helloService", HelloService.class);
         String result = helloService.say("world");
         System.out.println(result);
     }
