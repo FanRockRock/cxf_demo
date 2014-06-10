8355e358e278564bc40c61c27ef8b4f591b8ddbe
黄勇 <huangyong.java@gmail.com>
2014-06-10 21:56:05 +0800

    【F】补充了几个 soap 实例

---
+++ soap_spring_cxf_annotation/src/main/java/demo/cxf/soap_spring_cxf_interceptor_annotation/HelloServiceImpl.java
@@ -0,0 +1,11 @@
+package demo.cxf.soap_spring_cxf_interceptor_annotation;
+
+import javax.jws.WebService;
+
+@WebService
+public class HelloServiceImpl implements HelloService {
+
+    public String say(String name) {
+        return "hello " + name;
+    }
+}
