e5fae739905a3f5c522c43fffadbc31f9314dd29
黄勇 <huangyong.java@gmail.com>
2014-06-17 00:25:43 +0800

    【I】代码优化

--- rest_spring_cxf_shiro/src/main/java/demo/cxf/rest_spring_cxf_shiro/ProductService.java
+++ rest_spring_cxf_shiro/src/main/java/demo/cxf/rest_spring_cxf_shiro/ProductService.java
@@ -11,41 +11,42 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
+import static javax.ws.rs.core.MediaType.*;
 
 @Path("/ProductService")
 public interface ProductService {
 
     @GET
     @Path("/products")
-    @Produces(MediaType.APPLICATION_JSON)
+    @Produces(APPLICATION_JSON)
     @RequiresPermissions("product.r")
     List<Product> retrieveAllProducts();
 
     @GET
     @Path("/product/{id}")
-    @Produces(MediaType.APPLICATION_JSON)
+    @Produces(APPLICATION_JSON)
     @RequiresPermissions("product.r")
     Product retrieveProductById(@PathParam("id") long id);
 
     @POST
     @Path("/products")
-    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(APPLICATION_FORM_URLENCODED)
+    @Produces(APPLICATION_JSON)
     @RequiresPermissions("product.r")
     List<Product> retrieveProductsByName(@FormParam("name") String name);
 
     @POST
     @Path("/product")
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(APPLICATION_JSON)
+    @Produces(APPLICATION_JSON)
     @RequiresPermissions("product.c")
     Product createProduct(Product product);
 
     @PUT
     @Path("/product/{id}")
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(APPLICATION_JSON)
+    @Produces(APPLICATION_JSON)
     @RequiresPermissions("product.u")
     Product updateProductById(@PathParam("id") long id, Map<String, Object> fieldMap);
 
